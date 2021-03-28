a5ef810aaa90d68baded55292b28b2839fab7520
niazmurshedsiam <niazmurshed16@gmail.com>
2021-03-28 21:37:37 +0600

    Send data to database and store in mongodb

---
+++ index.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const app = express();
+app.use(cors());
+app.use(bodyParser.json());
+const port = 5000;
+
+const MongoClient = require("mongodb").MongoClient;
+const uri =
+  "mongodb+srv://arabian:gVwTMU5dC3e0sFWh@cluster0.bm8mo.mongodb.net/burjAlArab?retryWrites=true&w=majority";
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+client.connect((err) => {
+  const bookings = client.db("burjAlArab").collection("bookings");
+  app.post("/addBooking", (req, res) => {
+    const newBooking = req.body;
+    bookings.insertOne(newBooking).then((result) => {
+      console.log(result);
+      res.send(result.insertedCount > 0);
+    });
+    console.log(newBooking);
+  });
+});
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(port);
