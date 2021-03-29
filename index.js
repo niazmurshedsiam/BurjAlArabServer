86b88c1df8e7adc63798ff5a5dcfdbf5f9db2ca1
niazmurshedsiam <niazmurshed16@gmail.com>
2021-03-29 12:11:37 +0600

    Verify jwt token in the server end using firebase admin

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
+const admin = require("firebase-admin");
 const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 const port = 5000;
+
+var serviceAccount = require("./burj-al-arab-after-auth-1e819-firebase-adminsdk-pxrwo-0f33ee5c0c.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+});
 
 const MongoClient = require("mongodb").MongoClient;
 const uri =
@@ -23,15 +30,31 @@ client.connect((err) => {
     });
     console.log(newBooking);
   });
-  app.get('/bookings',(req,res)=>{
-    console.log(req.headers.authorization);
-    bookings.find({email : req.query.email})
-    .toArray((err,documents)=>{
-      res.send(documents)
-    })
-  })
+  app.get("/bookings", (req, res) => {
+    const bearer = req.headers.authorization;
+    if (bearer && bearer.startsWith("Bearer")) {
+      const idToken = bearer.split(" ")[1];
+      console.log({ idToken });
+      admin
+        .auth()
+        .verifyIdToken(idToken)
+        .then(function (decodedToken) {
+          const tokenEmail = decodedToken.email;
+          const queryEmail = req.query.email;
+          console.log(tokenEmail,queryEmail);
+          if (tokenEmail == req.query.email) {
+            bookings
+              .find({ email: req.query.email })
+              .toArray((err, documents) => {
+                res.send(documents);
+              });
+          }
+        })
+        .catch(function (error) {
+          // Handle error
+        });
+    }
+  });
 });
-
-
 
 app.listen(port);
